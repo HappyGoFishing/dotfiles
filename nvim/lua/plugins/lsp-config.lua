80029cd3c9dde26d1c450cd720abce3bc61ef444
Kieran C <85656267+HappyGoFishing@users.noreply.github.com>
2025-05-05 18:30:33 +0100

    Update lsp-config.lua

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -27,14 +27,6 @@ return {
 
             lspconfig.rust_analyzer.setup({})
             lspconfig.clangd.setup({})
-            lspconfig.zls.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                cmd = { "zls" },
-                filetypes = { "zig", "zir" },
-                root_dir = lspconfig.util.root_pattern("zls.json", "build.zig", ".git"),
-                single_file_support = true,
-            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
