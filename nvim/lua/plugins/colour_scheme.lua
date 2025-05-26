b613482cefca3750d601c35150ba19325d6f48bf
HappyGoFishing <85656267+HappyGoFishing@users.noreply.github.com>
2025-05-26 04:19:18 +0100

    Update colour_scheme.lua

--- nvim/lua/plugins/colour_scheme.lua
+++ nvim/lua/plugins/colour_scheme.lua
@@ -1,12 +1,28 @@
-return  {
+return {
     'f4z3r/gruvbox-material.nvim',
-    priority = 1000 ,
+    priority = 1000,
     opts = ...,
     config = function()
         local builtin = require('gruvbox-material').setup({
-            transparent = true,
+            italics = true,             -- enable italics in general
+            contrast = "medium",        -- set contrast, can be any of "hard", "medium", "soft"
+            comments = {
+                italics = true,         -- enable italic comments
+            },
+            background = {
+                transparent = false,    -- set the background to transparent
+            },
+            float = {
+                force_background = false, -- force background on floats even when background.transparent is set
+                background_color = nil,   -- set color for float backgrounds. If nil, uses the default color set
+                                          -- by the color scheme
+            },
+            signs = {
+                highlight = true,       -- whether to highlight signs
+            },
+            customize = nil,            -- customize the theme in any way you desire, see below what this
+                                       -- configuration accepts
         })
         vim.cmd("colorscheme gruvbox-material")
     end
-
 }
