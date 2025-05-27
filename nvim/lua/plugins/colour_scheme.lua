a03323ed3c40d00b45487a3013b5c7033dc0a38c
HappyGoFishing <85656267+HappyGoFishing@users.noreply.github.com>
2025-05-27 04:47:14 +0100

    changed theme from vscode to gruvbox material dark

--- nvim/lua/plugins/colour_scheme.lua
+++ nvim/lua/plugins/colour_scheme.lua
@@ -1,12 +1,26 @@
-return  {
-    "Mofiqul/vscode.nvim",
-    priority = 1000 ,
+return {
+    'f4z3r/gruvbox-material.nvim',
+    priority = 1000,
     opts = ...,
     config = function()
-        local builtin = require('vscode').setup({
-            transparent = true,
+        local builtin = require('gruvbox-material').setup({
+            italics = true,               -- enable italics in general
+            contrast = "medium",          -- set contrast, can be any of "hard", "medium", "soft"
+            comments = {
+                italics = true,           -- enable italic comments
+            },
+            background = {
+                transparent = false,      -- set the background to transparent
+            },
+            float = {
+                force_background = false, -- force background on floats even when background.transparent is set
+                background_color = nil,   -- set color for float backgrounds. If nil, uses the default color set
+                                          -- by the color scheme
+            },
+            signs = {
+                highlight = true,         -- whether to highlight signs
+            },
+            customize = nil,
         })
-        vim.cmd("colorscheme vscode")
     end
-
 }
