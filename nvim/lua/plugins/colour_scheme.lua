9cf1838b4341715514e76677da49d31b7e12b528
HappyGoFishing <85656267+HappyGoFishing@users.noreply.github.com>
2025-10-10 03:12:59 +0100

    Update colour_scheme.lua

--- nvim/lua/plugins/colour_scheme.lua
+++ nvim/lua/plugins/colour_scheme.lua
@@ -1,26 +1,12 @@
-return {
-    'f4z3r/gruvbox-material.nvim',
-    priority = 1000,
+return  {
+    "Mofiqul/vscode.nvim",
+    priority = 1000 ,
     opts = ...,
     config = function()
-        local builtin = require('gruvbox-material').setup({
-            italics = true,               -- enable italics in general
-            contrast = "medium",          -- set contrast, can be any of "hard", "medium", "soft"
-            comments = {
-                italics = true,           -- enable italic comments
-            },
-            background = {
-                transparent = false,      -- set the background to transparent
-            },
-            float = {
-                force_background = false, -- force background on floats even when background.transparent is set
-                background_color = nil,   -- set color for float backgrounds. If nil, uses the default color set
-                                          -- by the color scheme
-            },
-            signs = {
-                force_background = true,         -- whether to highlight signs
-            },
-            customize = nil,
+        local builtin = require('vscode').setup({
+            transparent = true,
         })
+        vim.cmd("colorscheme vscode")
     end
+
 }
